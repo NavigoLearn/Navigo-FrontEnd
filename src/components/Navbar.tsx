1f586439358964dedf6b6ce19c1b82c6afc03a00
antonel-iwnl <antonioadelin@gmail.com>
2023-03-26 12:43:34 +0300

    26/03/2023 12:40 PM

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,20 @@
-import React from "react";
-import {useState} from 'react';
-import GuestNavbar from "./GuestNavbar";
-import LoggedInNavbar from "./LoggedInNavbar";
-
+import React, { useState } from 'react';
+import GuestNavbar from './GuestNavbar';
+import LoggedInNavbar from './LoggedInNavbar';
 
 // const [isLoggedIn, setIsLoggedIn] = useState(false);
 const isLoggedIn = true;
 
 function handleLogin() {
-    // implement later
+  // implement later
 }
 
 const Navbar = () => {
-    return (
-        <nav>
-        <ul>
-            {isLoggedIn ? (
-            <>
-                <LoggedInNavbar />
-            </>
-            ) : (
-                <GuestNavbar />
-            )}
-        </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul>{isLoggedIn ? <LoggedInNavbar /> : <GuestNavbar />}</ul>
+    </nav>
+  );
 };
 
 export default Navbar;
