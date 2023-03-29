5891573f56b0ce811b7b60d2ef75ccc7a480d7c3
erupturatis <bb.octav@gmail.com>
2023-03-29 21:42:40 +0300

    (create)[Wed 03/29/2023] tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,18 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    colors: {
-      primary: '#3361D8',
-      secondary: '#3C42CE',
-      highlight: '#007aff',
-      background: '#ECEFF2',
-      textmain: '#000000',
-      textsecondary: '#000000/60',
-      subtext: 'rgba(0, 0, 0, 0.5)',
-      textgradient: 'linear-gradient(90deg, #561BB6 0%, #253FC8 100%)',
-      buttongradient: 'linear-gradient(90deg, #3361D8 0%, #262EE7 100%)',
+    extend: {
+      colors: {
+        primary: '#3361D8',
+        secondary: '#3C42CE',
+        highlight: '#007aff',
+        background: '#ECEFF2',
+        textgradient: 'linear-gradient(90deg, #561BB6 0%, #253FC8 100%)',
+        buttongradient: 'linear-gradient(90deg, #3361D8 0%, #262EE7 100%)',
+        white: '#FFFFFF',
+      },
+      textColor: {
+        main: 'rgb(0,0,0)',
+        secondary: 'rgb(0,0,0,0.6)',
+        placeholder: 'rgb(0,0,0,0.3)',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
