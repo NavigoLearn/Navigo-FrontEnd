77f47930cbd6c392984cdf3cc08568fe35697456
erupturatis <bb.octav@gmail.com>
2023-03-29 15:24:47 +0300

    (fix)[Wed 03/29/2023]  preserving rest of colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,18 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    colors: {
-      primary: '#3361D8',
-      secondary: '#3C42CE',
-      highlight: '#007aff',
-      background: '#ECEFF2',
-      textmain: '#000000',
-      textsecondary: '#000000/60',
-      subtext: 'rgba(0, 0, 0, 0.5)',
-      textgradient: 'linear-gradient(90deg, #561BB6 0%, #253FC8 100%)',
-      buttongradient: 'linear-gradient(90deg, #3361D8 0%, #262EE7 100%)',
+    extend: {
+      colors: {
+        primary: '#3361D8',
+        secondary: '#3C42CE',
+        highlight: '#007aff',
+        background: '#ECEFF2',
+        textmain: '#000000',
+        textsecondary: '#000000/60',
+        subtext: 'rgba(0, 0, 0, 0.5)',
+        textgradient: 'linear-gradient(90deg, #561BB6 0%, #253FC8 100%)',
+        buttongradient: 'linear-gradient(90deg, #3361D8 0%, #262EE7 100%)',
+        white: '#FFFFFF',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
