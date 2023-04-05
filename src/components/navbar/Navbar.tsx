bc1629294c8c1b9b1dd38731b23f775bb4c09633
erupturatis <bb.octav@gmail.com>
2023-04-05 10:35:17 +0300

    (fix)[Wed 04/05/2023]  navbar responsive fast

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,29 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import DesktopNavbar from './DesktopNavbar';
 import MobileNavbar from './MobileNavbar';
 
 const Navbar = () => {
-  const [isDesktop, setIsDesktop] = useState(undefined);
-
+  let mediaQuery;
   useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth >= 950);
-    };
-
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
+    mediaQuery = window.matchMedia('(max-width: 950px)');
   }, []);
 
   return (
-    <nav className='z-100'>
-      <ul>
-        {typeof isDesktop !== 'undefined' &&
-          (isDesktop ? <DesktopNavbar /> : <MobileNavbar />)}
-      </ul>
-    </nav>
+    <div className='z-100'>
+      {!mediaQuery ? <DesktopNavbar /> : <MobileNavbar />}
+    </div>
   );
 };
 
