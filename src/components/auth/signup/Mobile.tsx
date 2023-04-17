15a77169d9bf33872832835cf1fceffcde606d99
antonel-iwnl <antonioadelin@gmail.com>
2023-04-17 11:37:07 +0300

    [asbg] ma sfade eugen

--- src/components/auth/signup/Mobile.tsx
+++ src/components/auth/signup/Mobile.tsx
@@ -1,9 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import {
+  GoogleLoginButton,
+  GithubLoginButton,
+} from 'react-social-login-buttons';
+import {
+  handleGoogleLogin,
+  handleGitHubLogin,
+} from '@components/auth/socialAuth';
 
+// Aici trebuie rescrise testele pentru ca name field a fost sters din register
 const MobileSignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [reapeatPassword, setRepeatPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    if (password !== reapeatPassword) {
+      alert('The passwords do not match!');
+      setPassword('');
+      setRepeatPassword('');
+      return;
+    }
+    e.preventDefault();
+    // console.log(name, email, password, reapeatPassword);
+    setRepeatPassword('');
+    setPassword('');
+    setEmail('');
+  };
+
   return (
-    <div>
-      <h1>MobileSignUp</h1>
+    <div className='mt-28'>
+      <h1>Welcome back!</h1>
     </div>
   );
 };
