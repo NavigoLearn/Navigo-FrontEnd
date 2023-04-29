e15de8038a8a5e706328e69cc6897bf60dbb590a
sopy <doimpt@sopy.one>
2023-04-29 04:30:07 +0300

    Fixing stuff because apple (currently relying on throttled chunking manager please help)

--- src/components/roadmap/nodes/misc/Tooltip.tsx
+++ src/components/roadmap/nodes/misc/Tooltip.tsx
@@ -1,8 +1,18 @@
+import * as d3 from 'd3';
 import React, { useState, useEffect } from 'react';
 import { useToolTip } from '@store/runtime/miscParams';
 import { setTriggerTooltip } from '@store/runtime/rerenderTriggers';
+import { useStore } from "@nanostores/react";
+import viewportCoord from "@store/runtime/viewport-coords";
 
 const Tooltip = ({ id }: { id: string }) => {
+  // get the scale from the store (throttled, so a better way should be found)
+  const { scale } = useStore(viewportCoord);
+
+  const isSafari =
+    navigator.userAgent.indexOf('Safari') != -1 &&
+    navigator.userAgent.indexOf('Chrome') == -1;
+
   // render tells if the tooltip should be rendered again
   const [render, setRender] = useState(false);
   useEffect(() => {
@@ -15,7 +25,18 @@ const Tooltip = ({ id }: { id: string }) => {
   const options = useToolTip(id);
   if (!options) return null;
 
-  return <div className='absolute bottom-0 w-full'>{options()}</div>;
+  return (
+    <div
+      className={`fixed bottom-0 w-full`}
+      style={isSafari
+        ? {
+        transform:`scale(${scale})`,
+        transformOrigin: 'top left'
+      } : {}}
+    >
+      {options()}
+    </div>
+  );
 };
 
 export default Tooltip;
