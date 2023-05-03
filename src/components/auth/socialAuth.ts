07ca1f59f8b872f71ab1236a73fdf387ac011bb3
sopy <doimpt@sopy.one>
2023-05-03 03:45:57 +0300

    Revert "test"
    This reverts commit 419d7ec9d28f89ae4853b3ec2788659ddc646b89.

--- src/components/auth/socialAuth.ts
+++ src/components/auth/socialAuth.ts
@@ -1,64 +1,58 @@
 function handleSocialLogin(link: string, features: string) {
-  // open custom html in a new window and store the window reference
-  const popup = window.open("/", '_blank', features);
+  // open /api/auth/google-login in a new window and store the window reference
+  const popup = window.open(link, '_blank', features);
+  // attach a listener to the child window's load event
+  popup.addEventListener('load', () => {
+    // check if cookie is set
+    if (!document.cookie.includes('token')) return;
 
-  function loginSuccess() {
-    // do something after login
-    console.log('login success');
+    // add closed event listener to the child window
+    popup.addEventListener('beforeunload', () => {
+      // do something before closing the child window
+      console.log('Closing login window...');
+
+      // redirect to home page
+      window.location.href = '/home';
+    });
 
     // close the child window
     popup.close();
 
-    // redirect to the home page
-    window.location.href = '/';
-  }
-  // write custom html to the child window
-  popup.document.write(`
-    <html>
-      <head>
-        <title>Authenticating...</title>
-        
-        <style>
-          body {
-            margin: 0;
-          }
-          
-          iframe {
-            width: 100vw;
-            height: 100vh;
-            border: none;
-            max-width: 100%;
-          }
-        </style>
-        
-        <script>
-        // get iframe element
-        const iframe = document.getElementById('loginframe');
-        //wait for the child window to get to the local domain
-        const intervalId = setInterval(() => {
-          try {
-            // if the child window's location is not the same as the parent window's location
-            if (popup.location.href !== window.location.href) {
-                return;
-            }
-            
-            // check if cookie is set
-            if (!document.cookie.includes('token')) return;
-            
-            // call parent window's function
-            window.opener.loginSuccess();
-          } catch (e) {
-            // if the child window is not on the same domain as the parent window
-            // then the child window's location will throw an error
-            return;
-          }
-        }, 100);
-        </script>
-      </head>
-      <body>
-        <iframe src="${link}" id="loginframe"></iframe>
-      </body>
-    </html>`);
+    // do something after successful login
+    console.log('Login successful!');
+  });
+
+  // poll the child window's location every 500ms
+  const intervalId = setInterval(() => {
+    try {
+      // if the child window's location is not the same as the parent window's location
+      if (popup.location.href !== window.location.href) {
+        // check if cookie is set
+        if (!document.cookie.includes('token')) return;
+
+        // add closed event listener to the child window
+        popup.addEventListener('beforeunload', () => {
+          // do something before closing the child window
+          console.log('Closing login window...');
+
+          // redirect to home page
+          window.location.href = '/home';
+        });
+
+        // close the child window
+        popup.close();
+
+        // do something after successful login
+        console.log('Login successful!');
+
+        // stop polling
+        clearInterval(intervalId);
+      }
+    } catch (e) {
+      // do nothing
+      console.log(e);
+    }
+  }, 500);
 }
 
 export function handleGoogleLogin() {
