1f657a50b531e89326b17d1dbd992e6b85e23ffd
sopy <doimpt@sopy.one>
2023-05-03 03:45:57 +0300

    more fixes

--- src/components/auth/socialAuth.ts
+++ src/components/auth/socialAuth.ts
@@ -1,58 +1,39 @@
 function handleSocialLogin(link: string, features: string) {
   // open /api/auth/google-login in a new window and store the window reference
   const popup = window.open(link, '_blank', features);
-  // attach a listener to the child window's load event
-  popup.addEventListener('load', () => {
-    // check if cookie is set
-    if (!document.cookie.includes('token')) return;
-
-    // add closed event listener to the child window
-    popup.addEventListener('beforeunload', () => {
-      // do something before closing the child window
-      console.log('Closing login window...');
-
-      // redirect to home page
-      window.location.href = '/home';
-    });
-
-    // close the child window
-    popup.close();
-
-    // do something after successful login
-    console.log('Login successful!');
-  });
 
   // poll the child window's location every 500ms
   const intervalId = setInterval(() => {
     try {
-      // if the child window's location is not the same as the parent window's location
-      if (popup.location.href !== window.location.href) {
-        // check if cookie is set
-        if (!document.cookie.includes('token')) return;
+      // check if cookie is set
+      if (!document.cookie.includes('token')) return;
 
-        // add closed event listener to the child window
-        popup.addEventListener('beforeunload', () => {
-          // do something before closing the child window
-          console.log('Closing login window...');
+      // close the child window
+      popup.close();
 
-          // redirect to home page
-          window.location.href = '/home';
-        });
+      // do something after successful login
+      console.log('Login successful!');
 
-        // close the child window
-        popup.close();
+      // stop polling
+      clearInterval(intervalId);
 
-        // do something after successful login
-        console.log('Login successful!');
-
-        // stop polling
-        clearInterval(intervalId);
-      }
+      // redirect to home page
+      window.location.href = '/';
     } catch (e) {
       // do nothing
       console.log(e);
     }
-  }, 500);
+  }, 1);
+
+  // if the child window is closed, stop polling
+  popup.onclose = () => {
+    clearInterval(intervalId);
+  }
+
+  // if parent window is closed, close the child window
+  window.onunload = () => {
+    popup.close();
+  }
 }
 
 export function handleGoogleLogin() {
@@ -60,10 +41,9 @@ export function handleGoogleLogin() {
   const { width, height } = window.screen;
 
   const top = height / 2 - 350;
-  const left = width / 2 - 225;
+  const left = width / 2 - 250;
 
-  const features = `scrollbars=no,resizable=no,status=no,location=no,
-  toolbar=no,menubar=no,width=500,height=700,left=${left},top=${top}`;
+  const features = `popup=1,width=500,height=700,left=${left},top=${top}`;
 
   // open /api-wrapper/auth/google-login in a new window and store the window reference
   handleSocialLogin('/api/auth/google-login', features);
@@ -76,8 +56,7 @@ export function handleGitHubLogin() {
   const top = height / 2 - 350;
   const left = width / 2 - 250;
 
-  const features = `scrollbars=no,resizable=no,status=no,location=no,
-  toolbar=no,menubar=no,width=500,height=700,left=${left},top=${top}`;
+  const features = `popup=1,width=500,height=700,left=${left},top=${top}`;
 
   handleSocialLogin('/api/auth/github-login', features);
 }
