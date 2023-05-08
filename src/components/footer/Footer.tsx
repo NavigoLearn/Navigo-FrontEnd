5f459f9ab34f3e788ff33b4f85891a735c0b3814
antonel-iwnl <antonioadelin@gmail.com>
2023-05-08 14:56:02 +0300

    [Mon 08/04/2023] (Fix) Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,14 +4,14 @@ import Socials from './Socials';
 
 const Footer = () => {
   return (
-    <div className=' h-64 md:h-72 w-full absolute bg-footer bottom-0 flex flex-col'>
-      <h1 className='text-white flex text-xl md:text-2xl font-roboto-text font-medium text-center mt-6 justify-center mx-auto'>
+    <div className='h-32 w-full absolute bg-footer bottom-0 flex flex-col'>
+      {/* <h1 className='text-white flex text-lg font-roboto-text font-medium text-center justify-center mx-auto'>
         Navigo Learn
-      </h1>
+      </h1> */}
       <Links />
       <Socials />
-      <hr className='mt-6 w-10/12 border-gray-600 flex justify-center mx-auto' />
-      <h1 className='font-roboto-text text-center text-base font-normal text-[#FFFFFF4D] mt-4'>
+      <hr className=' w-10/12 border-gray-600 flex justify-center mx-auto mt-4' />
+      <h1 className='font-roboto-text text-center text-base font-normal text-[#FFFFFF4D] mt-1'>
         @copyright. All rights reserved
       </h1>
     </div>
--- src/components/footer/Linking.tsx
+++ src/components/footer/Linking.tsx
@@ -2,31 +2,31 @@ import React from 'react';
 
 const Links = () => {
   return (
-    <div className='mt-10 flex flex-row justify-center gap-6'>
+    <div className='flex flex-row justify-center gap-6 mt-4'>
       <a
         href='/feedback'
-        className='font-roboto-text text-normal text-white text-sm md:text-2xl text-center hover:underline hover:underline-offset-4 select-none'
+        className='font-roboto-text text-normal text-white text-md text-center hover:underline hover:underline-offset-4 select-none'
       >
         {' '}
         Feedback
       </a>
       <a
         href='/explore'
-        className='font-roboto-text text-normal text-white text-sm md:text-2xl text-center hover:underline hover:underline-offset-4 select-none'
+        className='font-roboto-text text-normal text-white text-md text-center hover:underline hover:underline-offset-4 select-none'
       >
         {' '}
         Explore
       </a>
       <a
         href='/home'
-        className='font-roboto-text text-normal text-white text-sm md:text-2xl text-center hover:underline hover:underline-offset-4 select-none'
+        className='font-roboto-text text-normal text-white text-md text-center hover:underline hover:underline-offset-4 select-none'
       >
         {' '}
         Home
       </a>
       <a
         href='/login'
-        className='font-roboto-text text-normal text-white text-sm md:text-2xl text-center hover:underline hover:underline-offset-4 select-none'
+        className='font-roboto-text text-normal text-white text-md text-center hover:underline hover:underline-offset-4 select-none'
       >
         {' '}
         Login
