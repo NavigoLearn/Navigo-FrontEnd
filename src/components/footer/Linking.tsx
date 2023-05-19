0aa61cd83a6cfd0847555506c950aab17f5c899f
sopy <doimpt@sopy.one>
2023-05-19 17:17:13 +0300

    Footer show profile button if user is logged in

--- src/components/footer/Linking.tsx
+++ src/components/footer/Linking.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import userStatus from '@store/user/user-status';
+import { useStore } from '@nanostores/react';
 
 const Links = () => {
+  // get logged user from userStatus.isLogged and make reactive
+  const { isLogged } = useStore(userStatus);
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    // avoid ssr error
+    setHydrated(true);
+  }, [isLogged]);
+
   return (
     <div className='flex flex-row justify-center gap-6 mt-4'>
       <a
@@ -24,13 +35,20 @@ const Links = () => {
         {' '}
         Home
       </a>
-      <a
-        href='/login'
-        className='font-roboto-text text-normal text-white text-md text-center hover:underline hover:underline-offset-4 select-none'
-      >
-        {' '}
-        Login
-      </a>
+      { hydrated &&  isLogged ?
+        (<a
+          href='/profile'
+          className='font-roboto-text text-normal text-white text-md text-center hover:underline hover:underline-offset-4 select-none'
+        >
+            {' '}
+            Profile
+        </a>) : (<a
+          href='/login'
+          className='font-roboto-text text-normal text-white text-md text-center hover:underline hover:underline-offset-4 select-none'
+        >
+          {' '}
+          Login
+        </a>)}
     </div>
   );
 };
