65154ddb3a3c7f4e0fe781f6f276a73e350f8c35
arking115 <rusalexandru115@gmail.com>
2023-07-12 12:13:33 +0300

    (Upgrade)[12.07.2023] Components tab getting improved waiting for factories

--- src/components/roadmap/editor/tabs/Components.tsx
+++ src/components/roadmap/editor/tabs/Components.tsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import roadmapPlaceholder from '@store/roadmap-refactor/data/roadmap-placeholder';
+import { useStore } from '@nanostores/react';
+import {
+  injectNewTitle,
+  injectNewDescription,
+} from '@src/typescript/roadmap_ref/node/core/data-mutation/inject';
+import TitleComponent from '@src/typescript/roadmap_ref/node/components/TitleComponent';
+import DescriptionComponent from '@src/typescript/roadmap_ref/node/components/DescriptionComponent';
+import { deepCopy } from '@src/typescript/roadmap/utils';
 
 const Components = () => {
-  return <div>Components</div>;
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const { nodes } = useStore(roadmapPlaceholder);
+
+  // const Title: TitleComponent = new TitleComponent(10, 20, 100, 50, title);
+  // const Description: DescriptionComponent = new DescriptionComponent(
+  //   10,
+  //   20,
+  //   100,
+  //   50,
+  //   description
+  // );
+
+  // const onSave = () => {
+  //   injectNewTitle(nodes[0], 'title', Title);
+  //   injectNewDescription(nodes[0], 'description', Description);
+  // };
+  console.log(deepCopy(roadmapPlaceholder.get().nodes));
+
+  return (
+    <div>
+      <h1>Title:</h1>
+      <input
+        type='text'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        id='title'
+        className='border border-black'
+      />
+      <h1>Description:</h1>
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        className='w-full h-96 border border-black'
+        id='description'
+      />
+      <button type='button'>Save</button>
+    </div>
+  );
 };
 
 export default Components;
